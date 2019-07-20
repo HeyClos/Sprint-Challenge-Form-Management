09b79e6535995e4763429cd1ebba7ce744c4fdd3
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-07-19 18:55:52 -0700

    GET request fixed

--- client/src/components/FormList.js
+++ client/src/components/FormList.js
@@ -2,25 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 const FormList = (props) => {
-    axios
-        .get("http://localhost:5000/api/restricted/data") 
-        
-        .then(res => {
-          console.log(res); 
-          // am i using my custom useInput hook in here correctly? Do i need to pass it res?
-          useInput();
-        })
-        .catch(err => {
-          console.log(err);
-        });
-
+        useInput(() => {
+            axiosWithAuth().get("http://localhost:5000/api/restricted/data")
+                .then(res => setFriends(res.data))
+                .catch(err => console.log(err))
+        }, [])
     return (
         <div>
             <ul className="Form-List">
                 {props.forms.map(form => {
                     return (
                     <li>
-                        {form.name}
+                        {form.username}
                     </li>
                     );
                 })}
